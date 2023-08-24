1fa12e95bcc56ce16f945cbff6fd5c1c5e7b6579
Gondoshiki <gondoshiki@gmail.com>
2023-08-24 13:04:13 +0700

    Add project files.

---
+++ SupplierRegistration/Model/FileProcess.js
@@ -0,0 +1,54 @@
+﻿$(document).ready(function () {
+
+    let searchParams = new URLSearchParams(window.location.search);
+    var id = null;
+    var id = searchParams.get('id');
+    $.ajax({
+        method: "POST",
+        //Call Function From url
+        url: "Service.asmx/FileProcess",
+        data: { ID: id },
+        dataType: "json",
+        beforeSend: function (data) {
+        },
+        success: function (server) {
+            var dt = JSON.parse(JSON.stringify(server))
+            //Command Table
+            //tbFile.style.width = "100%";
+            $('#FileList').show();
+            $('#FileList').DataTable({
+                order: [],
+                searching: false,
+                paging: false,
+                info: false,
+                responsive: true,
+                data: dt,
+                //"bDestroy": true,                    
+                columns: [
+                    // { data: 'FileName' },
+                    { data: 'Title' },
+                    { data: 'FileName' },
+                    { data: 'Source' },
+                ],
+            });
+        },
+
+    });
+    var CookieID = $('#Body_CreateID').val();
+    var CreateBy = $('#Body_CreateCom').val();
+    var UploadFile = $('#Body_UploadFile').val();
+    if (CookieID == CreateBy) {
+        $("#btnFinishModal").show(); $("#btnRejectModal").show(); $("#btnReviseModal").show();
+    }
+    else {
+        $("#btnFinishModal").hide(); $("#btnRejectModal").hide(); $("#btnReviseModal").hide();
+    }
+    if (UploadFile == 1) {
+        $("#cardUpload").show();
+    }
+    else {
+        $("#cardProcess").show();
+        $("#btnFinishModal").hide();
+        $("#btnReviseModal").hide();
+    }
+});
